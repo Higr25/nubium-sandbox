5ace304b02e9c3adc91b58c8fcf715c367ee170e
Václav Ryska <vaclav.ryska@cleanup.cloud>
2021-07-22 16:49:03 +0200

    fix sql charset

--- web-project/app/Models/PostModel.php
+++ web-project/app/Models/PostModel.php
@@ -13,84 +13,111 @@ class PostModel extends BaseModel {
      * @param string $order
      * @return object
      */
-    public function getPosts($paginator, $isLoggedIn, $order, $order_type, $idUser = null) {				
-								$base_logged = <<<EOSQL
-												SELECT p.*,
-																COALESCE( COUNT(distinct(vu.id)), 0 ) AS upvotes,
-																COALESCE( COUNT(distinct(vd.id)), 0 ) AS downvotes,
-																(COUNT(distinct(vu.id)) - COUNT(distinct(vd.id))) AS rating,
-																COALESCE( COUNT(distinct(v.id)), 0 ) AS voted,
-																v.up AS voted_up
-EOSQL;
-								
-								$base_not_logged = <<<EOSQL
-												SELECT p.*,
-																COALESCE( COUNT(distinct(vu.id)), 0 ) AS upvotes,
-																COALESCE( COUNT(distinct(vd.id)), 0 ) AS downvotes,
-																(COUNT(distinct(vu.id)) - COUNT(distinct(vd.id))) AS rating
-EOSQL;
-								
-								$sql_mid = <<<EOSQL
-												FROM t_post p
-
-												LEFT JOIN (SELECT id, id_post, count(*) AS COUNT
-																FROM t_vote vu
-																WHERE up = 1 AND active = 1
-																GROUP BY id) vu ON p.id = vu.id_post
-
-												LEFT JOIN (SELECT id, id_post, count(*) AS COUNT
-																FROM t_vote vd
-																WHERE up = 0 AND active = 1
-																GROUP BY id) vd ON p.id = vd.id_post
-EOSQL;
-								
-								$sql_logged = <<<EOSQL
-												LEFT JOIN (
-																SELECT id, id_post, up
-																FROM t_vote v
-																WHERE id_user = ? AND active = 1
-											) v ON p.id = v.id_post	
-EOSQL;
-				
-								$not_logged = <<<EOSQL
-												WHERE active = 1 AND private = 0
-EOSQL;
-								
-								$end = <<<EOSQL
-												GROUP BY p.id
-												ORDER BY ?
-												LIMIT ?, ?				
-EOSQL;
-								
-								$count = <<<EOSQL
-												SELECT p.id
-												FROM t_post p
-												WHERE active = 1				
-EOSQL;
-								
-								$count_not_logged = <<<EOSQL
-												AND private = 0				
-EOSQL;
+    public function getPosts($paginator, $isLoggedIn, $order, $order_type, $idUser = null) {										
 								$order_types = ['created_at', 'header', 'rating'];
 								
 								$_order = array_key_exists($order,	$order_types) ? $order_types[$order] : reset($order_types);
 								$_order_type = boolval($order_type);
 																
 								if ($isLoggedIn) {
-												$sql = $base_logged . ' ' . $sql_mid . ' ' . $sql_logged . ' ' . $end;
+												$sql = $this->getSqlLogged();
+												$count = $this->getSqlLogged(true);
 												
 												$rows = $this->db->query($sql, $idUser, [$_order => $_order_type], $paginator->getOffset(), $paginator->getLength())->fetchAll();
 												$rowsCount = $this->db->query($count)->getRowCount();
 								} else {
-												$sql = 	$base_not_logged . ' ' . $sql_mid . ' ' . $not_logged . ' ' . $end;
+												$sql = $this->getSqlNotLogged();
+												$count_not_logged = $this->getSqlNotLogged(true);
 												
 												$rows = $this->db->query($sql, [$_order => boolval($_order_type)], $paginator->getOffset(), $paginator->getLength())->fetchAll();
-												$rowsCount = $this->db->query($count . ' ' . $count_not_logged)->getRowCount();
+												$rowsCount = $this->db->query($count_not_logged)->getRowCount();
 								}
         
         return ['data' => $rows,
 												    'count' => $rowsCount];
     }
+				
+				private function getSqlLogged($count = null) {
+								$sql = <<<EOSQL
+								    SELECT p.*,
+																COALESCE( COUNT(distinct(vu.id)), 0 ) AS upvotes,
+																COALESCE( COUNT(distinct(vd.id)), 0 ) AS downvotes,
+																(COUNT(distinct(vu.id)) - COUNT(distinct(vd.id))) AS rating,
+																COALESCE( COUNT(distinct(v.id)), 0 ) AS voted,
+																v.up AS voted_up
+												FROM t_post p
+												LEFT JOIN (
+																SELECT id, id_post, count(*) AS COUNT
+																FROM t_vote vu
+																WHERE up = 1 AND active = 1
+																GROUP BY id
+												) vu ON p.id = vu.id_post
+												LEFT JOIN (
+																SELECT id, id_post, count(*) AS COUNT
+																FROM t_vote vd
+																WHERE up = 0 AND active = 1
+																GROUP BY id
+												) vd ON p.id = vd.id_post	
+												LEFT JOIN (
+																SELECT id, id_post, up
+																FROM t_vote v
+																WHERE id_user = ? AND active = 1
+											 ) v ON p.id = v.id_post	
+												GROUP BY p.id, v.up
+												ORDER BY ?
+												LIMIT ?, ?					
+EOSQL;
+								
+								$sql_count = <<<EOSQL
+												SELECT p.id
+												FROM t_post p
+												WHERE active = 1						
+EOSQL;
+								
+								if ($count) {
+												return $sql_count;
+								}
+								
+								return $sql;
+				}
+				
+				private function getSqlNotLogged($count = null) {
+								$sql = <<<EOSQL
+												SELECT p.*,
+																COALESCE( COUNT(distinct(vu.id)), 0 ) AS upvotes,
+																COALESCE( COUNT(distinct(vd.id)), 0 ) AS downvotes,
+																(COUNT(distinct(vu.id)) - COUNT(distinct(vd.id))) AS rating
+												FROM t_post p
+												LEFT JOIN (
+																SELECT id, id_post, count(*) AS COUNT
+																FROM t_vote vu
+																WHERE up = 1 AND active = 1
+																GROUP BY id
+												) vu ON p.id = vu.id_post
+												LEFT JOIN (
+																SELECT id, id_post, count(*) AS COUNT
+																FROM t_vote vd
+																WHERE up = 0 AND active = 1
+																GROUP BY id
+												) vd ON p.id = vd.id_post				
+												WHERE active = 1 AND private = 0			
+												GROUP BY p.id
+												ORDER BY ?
+												LIMIT ?, ?					
+EOSQL;
+								
+								$sql_count = <<<EOSQL
+												SELECT p.id
+												FROM t_post p
+												WHERE active = 1 AND private = 0	
+EOSQL;
+								
+								if ($count) {
+												return $sql_count;
+								}
+								
+								return $sql;
+				}
 				
 				public function insertVote($idPost, $up, $idUser, $created_ip) {
 								$this->db->table('t_vote')
